43bdd561d5b6598b3dd0435e9d9acddd26f32316
Marcin Chwedczuk <chwedczukm@yahoo.com>
2015-07-19 15:25:46 +0200

    s,v,j specifiers, precision, width.

--- src/format.js
+++ src/format.js
@@ -19,29 +19,101 @@
                            ' has no coresponding argument.');
             }
         };
+
+        var repeat = function(s, times) {
+            return new Array(times + 1).join(s);
+        };
         
-        return format.replace(/%([a-z])/g, function(match, spec) {
-            var arg;
+        var padLeft = function(s, width) {
+            if (s.length >= width) {
+                return s;
+            }
+            else {
+                var padding = repeat(' ', width - s.length);
+                return padding + s;
+            }
+        };
+
+        var padRight = function(s, width) {
+            if (s.length >= width) {
+                return s;
+            }
+            else {
+                var padding = repeat(' ', width - s.length);
+                return s + padding;
+            }
+        };
+
+        var hasFlag = function(flags, flag) {
+            return flags && (flags.indexOf(flag) !== (-1));
+        };
+
+        /*
+        var StringBasedSpecifier = function(spec) {
+            var self = this;
+
+            self.format = function(flags, width, precision, args) {
+            
+            };
+        };*/
+
+        var formatSpecifier = function(fullSpec, flags, width, precision, spec) {
+            var arg, result;
+
+            assertHasArg(fullSpec);
 
             switch(spec) {
             case 's':
-                assertHasArg(match);
-                return String(args.shift());
+                result = String(args.shift());
+                break;
             
             case 'v':
-                assertHasArg(match);
                 arg = args.shift();
 
                 if (arg && typeof(arg.valueOf) === "function") {
-                    return String(arg.valueOf());
+                    result = String(arg.valueOf());
                 }
                 else {
-                    return String(arg);
+                    result = String(arg);
                 }
                 break;
 
+            case 'j':
+                result = JSON.stringify(args.shift());
+                break;
+
+            case 'i': case 'd':
+                result = String(Number(args.shift()));
+                break;
+
             default:
+                // unknown specifier
+                return fullSpec;
+            }
+
+            if (precision) {
+                result = result.substring(0, Number(precision));
+            }
+
+            if (width) {
+                var padFunction = (hasFlag(flags, '-') ? padRight : padLeft);
+                result = padFunction(result, Number(width));
+            }
+
+            return result;
+        };
+
+        // format: %[flags][width][.precision][length]specifier
+        // based on: http://www.cplusplus.com/reference/cstdio/printf
+        return format.replace(/%([-])?(\d+|\*)?(?:\.(\d+))?([a-zA-Z])/g, 
+        function(match, flags, width, precision, spec) {
+            var KNOWN_SPECIFIERS = 'svjdi';
+
+            if (KNOWN_SPECIFIERS.indexOf(spec) === (-1)) {
                 return match;
+            }
+            else {
+                return formatSpecifier(match, flags, width, precision, spec);
             }
         });
     };
--- test/format.spec.js
+++ test/format.spec.js
@@ -7,6 +7,12 @@
 
     var format = require('../src/format.js').format;
 
+    var constFn = function(constant) {
+        return function() {
+            return constant;
+        };
+    };
+
     describe('format()', function() {
         it('throws exception given non-string format', function() {
             expect(format.bind(null, 33)).to.throw();
@@ -31,28 +37,36 @@
                 .should.throw();
         });
 
+        it('ignores excessive arguments', function() {
+            format.bind(null, '%s', 'foo', 'bar', 'nyu')
+                .should.not.throw();
+        });
+
         describe('%s specifier', function() {
             it('replaces specifier with String()\'ed version of argument', function() {
                 format('[%s]', 'foo').should.equal('[foo]');
                 format('[%s]', null).should.equal('[null]');
                 format('[%s]', undefined).should.equal('[undefined]');
                 format('[%s]', {}).should.equal('[[object Object]]');
-                format('[%s]', { toString: function() { return 'ok'; } })
+                format('[%s]', { toString: constFn('ok') })
                     .should.equal('[ok]');
             });
         });
 
-        describe('%v specifier', function() {
+        describe('%v specifier (valueOf)', function() {
             it('replaces specifier with String()\'ed version of value ' + 
-               'returned by valueOf() method', function() {
+               'returned by valueOf() method of argument', function() {
                 
                 // Object(33) creates Number wrapper around 33
                 format('[%v]', Object(33))
                     .should.equal('[33]');
+
+                format('[%v]', { valueOf: constFn(3) })
+                    .should.equal('[3]');
             });
 
             it('behaves as %s when argument has no valueOf() method', function() {
-                var obj = { valueOf: null, toString: function() { return 'foo'; } };
+                var obj = { valueOf: null, toString: constFn('foo') };
 
                 format('[%v]', obj)
                     .should.equal('[foo]');
@@ -61,6 +75,105 @@
             it('behaves as %s when argument is null or undefined', function() {
                 format('[%v]', null)
                     .should.equal('[null]');
+            });
+
+            it('works when valueOf() returns null or undefined', function() {
+                format('[%v]', { valueOf: constFn(null) })
+                    .should.equal('[null]');
+
+                format('[%v]', { valueOf: constFn(undefined) })
+                    .should.equal('[undefined]');
+            });
+        });
+
+        describe('%j specifier (JSON)', function() {
+            it('replaces specifier with JSON.stringify()\'ed ' + 
+               'version of argument', function() {
+                
+                format('[%j]', { foo: 1 })
+                    .should.equal('[{"foo":1}]');
+
+                format('[%j]', true)
+                    .should.equal('[true]');
+
+                format('[%j]', 1)
+                    .should.equal('[1]');
+
+                // NaN's are not permitted in JSON, they are
+                // serialized as null's
+                format('[%j]', NaN)
+                    .should.equal('[null]');
+            });
+        });
+
+        describe('%i or %d specifier (integer)', function() {
+            it('prints integer part of Number()\'ed argument', function() {
+                format('[%d]', 33)
+                    .should.equal('[33]');
+
+                format('[%i]', 71)
+                    .should.equal('[71]');
+
+                format('[%d]', { valueOf: constFn(123) })
+                    .should.equal('[123]');
+            });
+        });
+
+        describe('width specifier', function() {
+            it('specifies minimum field width (too short fields are padded ' + 
+               'with spaces on the right)', function() {
+            
+                format('%10s', 'foo')
+                    .should.equal('       foo');
+
+                format('%10s', 'x')
+                    .should.equal('         x');
+
+                format('%3s', 'foo')
+                    .should.equal('foo');
+
+                format('%5s', 'mymymy')
+                    .should.equal('mymymy');
+            });
+
+            it('when minus flag is specified field are padded on the left', function() {
+                format('%-10s', 'foo')
+                    .should.equal('foo       ');
+
+                format('%-10s', 'x')
+                    .should.equal('x         ');
+
+                format('%-3s', 'foo')
+                    .should.equal('foo');
+
+                format('%-5s', 'mymymy')
+                    .should.equal('mymymy');
+            });
+        });
+
+        describe('precision specifier', function() {
+            it('given %s,%v,%j specifiers limits number of character printed', function() {
+                format('[%.3s]', 'foo')
+                    .should.equal('[foo]');
+
+                format('[%.3s]', 'foozble')
+                    .should.equal('[foo]');
+
+                format('[%.3s]', 'fo')
+                    .should.equal('[fo]');
+
+                format('[%.5v]', { valueOf: constFn(123456) })
+                    .should.equal('[12345]');
+
+                format('[%.1j]', { foo: 1 })
+                    .should.equal('[{]');
+            });
+        });
+
+        describe('integration tests', function() {
+            it('allows to use complex specifiers', function() {
+                format('%5.3s', 'zzzkkkxxx')
+                    .should.equal('  zzz');
             });
         });
 
